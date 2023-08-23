26d1cc433f091c61aca1dd7e0133dc04d10589c1
haih19 <haih19c@gmail.com>
2023-08-24 01:09:44 +0700

    feat: carousel

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,15 @@
+<template>
+   <div class="card max-w-[150px] p-1 bg-white">
+      <p
+         class="mb-2 text-md tracking-tight text-gray-900 dark:text-white text-center">
+         Noteworthy <slot></slot>
+      </p>
+   </div>
+</template>
+
+<style scoped lang="scss">
+.card {
+   border-radius: 25px;
+   border: 1px solid #ededed;
+}
+</style>
---
+++ src/components/CarouselComponent.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import CardComponent from "./CardComponent.vue";
+import {Swiper, SwiperSlide} from "swiper/vue";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css";
+
+const onSwiper = (swiper: any) => {
+   console.log(swiper);
+};
+const onSlideChange = () => {
+   console.log("slide change");
+};
+</script>
+<template>
+   <div>
+      <Swiper
+         :centerInsufficientSlides="true"
+         slidesPerView="auto"
+         :spaceBetween="16"
+         :enabled="true"
+         :observer="true"
+         :observeParents="true"
+         :centeredSlides="true"
+         :parallax="true"
+         @swiper="() => onSwiper()"
+         @slideChange="onSlideChange">
+         <SwiperSlide
+            v-for="item in 10"
+            :key="item"
+            v-slot="{isActive}">
+            <CardComponent>{{ isActive ? item : "InActive" }}</CardComponent>
+         </SwiperSlide>
+      </Swiper>
+   </div>
+</template>
+
+<style scoped lang="scss">
+.swiper {
+   .swiper-wrapper {
+      .swiper-slide {
+         width: 150px;
+         height: auto;
+      }
+   }
+}
+</style>
